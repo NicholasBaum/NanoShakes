24fdb720469f437a39878e09f29136f478687bf9
NB <n.baumgaertner@hotmail.com>
2023-01-25 19:39:18 +0100

    removed old model

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 from nanoshakes_model import NanoShakes
-from ng_gpt_model import NgGptModel
 import time
 from datetime import timedelta
 import torch
@@ -85,8 +84,6 @@ def calcAverageError(model):
 print("Using " + device)
 model = NanoShakes(vocab_size=vocab_size, input_size=input_size, embd_size=embd_size,
                    layer_count=layer_count, head_count=head_count, dropout=dropout, device=device)
-# model = NgGptModel(vocab_size=vocab_size, n_embd=embd_size, block_size=input_size,
-#                            n_layer=layer_count, n_head=head_count, dropout=dropout)
 
 model.to(device)
 print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
